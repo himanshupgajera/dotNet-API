fdf578fabc6e7d74c023c4e646ffbb192b29e14d
Himanshu <divine.developer2024@gmail.com>
2025-03-17 15:45:26 +0530

    initial release

---
+++ DemoAPI/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students = new List<Student>();
+
+        // GET
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        // GET by Id
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        // POST 
+        [HttpPost]
+        public void Post([FromBody]Student value)
+        {
+            students.Add(value);
+        }
+
+        // PUT 
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Student value)
+        {
+            int i = students.FindIndex(s => s.Id == id);
+            if (i > 0)
+            {
+                students[i] = value;
+            }
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            students.RemoveAll(s => s.Id == id);
+        }
+    }
+}
---
+++ DemoAPI/Student.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace DemoAPI
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
